f9015f07764734b55837bba8b20b18a6f0dd472c
gvmossato_loyall <gabriel@loyall.com.br>
2022-10-14 11:46:19 -0300

    finishes 1.3

--- src/client.py
+++ src/client.py
@@ -1,5 +1,7 @@
+import argparse
 import socket
 import io
+import string
 
 from stegano import lsb
 
@@ -8,16 +10,41 @@ HOST = '127.0.0.1'
 PORT = 50007
 
 
-output_path = "assets\\secret-img.png"
+def get_cmd_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s",
+        "--secret",
+        type=str,
+        required=True,
+        help="secret message to hide"
+    )
+    parser.add_argument(
+        "-i",
+        "--image",
+        type=str,
+        required=True,
+        help="path to image that will hide the secret"
+    )
+    return parser.parse_args()
+
+
+output_path = './assets/secret-img.png'
+args = get_cmd_args()
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: # IPv4 and TCP
     s.connect((HOST, PORT))
-    data = s.recv(int(1e7)) # Images up to 10 MB
+
+    send_data = str.encode(f"STEGANO {args.image}\n{args.secret}")
+    s.send(send_data)
+
+    received_data = s.recv(int(1e7)) # Images up to 10 MB
 
     with io.open(output_path, "wb") as download_img:
-        download_img.write(data)
+        download_img.write(received_data)
 
     s.shutdown(socket.SHUT_RDWR)
     s.close()
 
-print('Segredo revelado:', lsb.reveal(output_path))
+print('Revealed secret:', lsb.reveal(output_path))
--- src/server.py
+++ src/server.py
@@ -15,22 +15,34 @@ def img2bytes(image, format):
     image.save(img_byte_arr, format=format)
     return img_byte_arr.getvalue()
 
+def parse_msg(byte_str):
+    client_msg = byte_str.decode('utf-8')
+
+    lines = client_msg.split('\n')
+    image_path = lines[0].split(' ')[-1]
+    secret = lines[1]
+    return client_msg, image_path, secret
+
 
 img_path = f"assets\\{IMG_FILENAME}"
-img_format = IMG_FILENAME.split('.')[-1]
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: # IPv4 and TCP
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
 
-    print(f'Servidor escutando em: {HOST}:{PORT}\n')
+    print(f'Server listening to {HOST}:{PORT}\n')
 
     while True:
-        conn, addr = s.accept() # Await for connection
+        conn, addr = s.accept()
+        received_data = conn.recv(1024)
 
-        print(f'Cliente conectado: {addr[0]}:{addr[1]}')
+        client_msg, image_path, secret = parse_msg(received_data)
+        print(f"Message received from client {addr[0]}:{addr[1]}:\n{client_msg}\n")
 
-        data = img2bytes(lsb.hide(img_path, SECRET), img_format)
-        conn.sendall(data)
+        img_format = image_path.split('.')[-1]
+        send_data = img2bytes(lsb.hide(image_path, secret), img_format)
+
+        conn.sendall(send_data)
         conn.shutdown(socket.SHUT_RDWR)
         conn.close()
